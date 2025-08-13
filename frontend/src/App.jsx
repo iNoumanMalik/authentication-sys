f1053b948272d6ae40403523e79b6cba0a27f9b9
Nouman Malik <inoumanmalik@outlook.com>
2025-08-13 16:07:35 +0500

    Initial files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Register from './components/register'
+
+function App() {
+  
+  return (
+    <div className='bg-[#8be2e9] min-h-screen flex justify-center items-center'>
+    {/* <h1  className=''>Authentication System</h1> */}
+    <Register/>
+    </div>
+  )
+}
+
+export default App
