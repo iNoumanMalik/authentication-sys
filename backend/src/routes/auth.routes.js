e241f79f2fb7906235dd24b33b06dd17b6b33d53
Nouman Malik <inoumanmalik@outlook.com>
2025-08-24 00:45:29 +0500

    setup routes for reset and forgot password handlers

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,12 +1,22 @@
-import {Router} from 'express'
-import {register,verifyEmail,login,logout,resendVerification} from '../controllers/auth.controller'
+import { Router } from "express";
+import {
+  register,
+  verifyEmail,
+  login,
+  logout,
+  resendVerification,
+  forgetPassword,
+  resetPassword,
+} from "../controllers/auth.controller";
 
 const router = Router();
 
-router.post('/register',register);
-router.post('/verify',verifyEmail);
-router.post('/login',login);
-router.post('/logout',logout);
-router.post('/resend-verification',resendVerification);
+router.post("/register", register);
+router.post("/verify", verifyEmail);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/resend-verification", resendVerification);
+router.post("/forget-password", forgetPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
